3e61efba81844fd4d58769d552530b0bb0830aa6
harbar20 <harbar2021@gmail.com>
2021-07-12 00:30:59 -0400

    colors bot complete

---
+++ colors/main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import re
+import requests
+from dotenv import load_dotenv
+import os
+from PIL import Image, ImageColor
+from io import BytesIO
+import asyncio
+
+load_dotenv()
+bot = commands.Bot(command_prefix="c!")
+token = os.environ.get("TOKEN")
+
+@bot.event
+async def on_ready():
+    print("Colors is ready!")
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        hexRegexs = re.findall("#[A-Fa-f0-9]{6}", message.content)
+        if hexRegexs:
+            files = []
+            for hexCode in hexRegexs:
+                img = Image.new("RGB", (32, 32), ImageColor.getrgb(hexCode))
+                with BytesIO() as image_binary:
+                    img.save(image_binary, 'PNG')
+                    image_binary.seek(0)
+                    files.append(discord.File(fp=image_binary, filename=f'{hexCode[1:]}.png'))
+            
+            await message.channel.send(files=files)
+
+        await bot.process_commands(message)
+
+@bot.command()
+async def color(ctx, *args):
+    for hexCode in args:
+        print(hexCode)
+    pass
+
+bot.run(token)
